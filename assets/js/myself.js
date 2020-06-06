0d32b9f56964340525f13f4d46efd866df04d5e2
KuangjuX <18630816527@163.com>
2020-06-06 16:39:26 +0800

    add English version

---
+++ assets/js/myself.js
@@ -0,0 +1,7 @@
+function transfer(url) {
+    /*window.location.href=url;*/
+    document.querySelector('body').style.opacity=0;
+    setTimeout(function () {
+        window.location.href=url;
+    },500);
+}
